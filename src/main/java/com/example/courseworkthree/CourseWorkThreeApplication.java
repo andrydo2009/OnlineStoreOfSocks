b1f0c32170d9c36488380d4a5c1b53571925892b
Андрей <andrydo2009@yandex.ru>
2023-03-23 23:38:13 +0300

    Begin!

---
+++ src/main/java/com/example/courseworkthree/CourseWorkThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworkthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( CourseWorkThreeApplication.class , args );
+    }
+
+}
---
+++ src/test/java/com/example/courseworkthree/CourseWorkThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworkthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkThreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
